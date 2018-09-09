761a93602f20b317e0924d13b7a46cc5e075b7dc
Dmitrii Zakharov <dzakharov@splunk.com>
2018-09-08 23:46:05 -0700

    Implemented hiding button and showing questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,30 @@
+
+
 function onlyOne(checkbox) {
     var checkboxes = document.getElementsByName('team')
     checkboxes.forEach((item) => {
         if (item !== checkbox) item.checked = false
     })
 }
+
+$(document).ready(function() {
+
+    
+
+    // var newHTML = '<div class="question">' +
+    //           '<h2 align="center">Which NBA Team won the most titles in the 90s?</h2>' +
+    //           '<input type="checkbox" name="team" value="NewYork" onclick="onlyOne(this)">New York Knicks</input>' +
+    //           '<input type="checkbox" name="team" value="Portland" onclick="onlyOne(this)">Portland Trailblazers</input>' +
+    //           '</div>'
+
+    $("#start_button").on('click', function() { 
+    alert("clicked on button");
+    //$("#start_button").replaceWith(newHTML);
+    $("#start_button").hide();
+    $("#questions").show();
+    });
+
+
+
+
+});
